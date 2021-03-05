84ce8065ad2f52661fccc07d7a84db5df68d6db9
rmarfatia <rmarfatia@pinterest.com>
2021-03-05 08:41:55 -0800

    Experimenting: Creating A Grid
    Built a grid with some tiles and played with some animations.

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,44 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.tileColor
+import com.example.androiddevchallenge.ui.theme.tileSelectedColor
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,12 +68,103 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
+val BOX_SIZE = 50.dp
+
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.DarkGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Grid()
     }
+}
+
+@Composable
+fun Grid(numItems: Int = 25, columns: Int = 5) {
+    Column(
+        modifier = Modifier.wrapContentSize(),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var items = numItems
+        while (items > 0) {
+            Row(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                for (i in 1..columns) {
+                    if (items == 0) break
+                    val index = numItems - items
+                    Tile(index, TWO[index])
+                    items--
+                }
+            }
+        }
+    }
+}
+
+val ONE = listOf(
+    0,1,1,0,0,
+    0,0,1,0,0,
+    0,0,1,0,0,
+    0,0,1,0,0,
+    0,1,1,1,0,
+)
+
+val TWO = listOf(
+    0,1,1,1,0,
+    0,0,0,1,0,
+    0,1,1,1,0,
+    0,1,0,0,0,
+    0,1,1,1,0,
+)
+
+
+@Composable fun Tile(index: Int = 0, on: Int = 0) {
+    val coroutineScope = rememberCoroutineScope()
+    val tileState = remember { mutableStateOf(0) }
+    val transition = updateTransition(targetState = tileState.value)
+    val color = transition.animateColor() { state ->
+        when (state) {
+            0 -> tileColor
+            else -> tileSelectedColor
+        }
+    }
+    val rotation = transition.animateFloat() { state ->
+        when (state) {
+            0 -> 0F
+            else -> 180F
+        }
+    }
+
+    coroutineScope.launch {
+        delay(100L * index)
+        tileState.value = on
+    }
+
+    Box(
+        modifier = Modifier
+            .width(BOX_SIZE)
+            .aspectRatio(1F)
+            .graphicsLayer(
+                rotationY = rotation.value
+            )
+            .drawBehind {
+                drawRect(color = color.value)
+            }
+            .clickable(interactionSource = MutableInteractionSource(), indication = null) {
+                if (tileState.value == 1) {
+                    tileState.value = 0
+                } else {
+                    tileState.value = 1
+                }
+            }
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
